c3c883fb4c9b87048feb57eaf301df0133aaf991
Zebratic <zebraticyt@gmail.com>
2023-11-30 13:20:45 +0100

    Update MinLaering.js

--- javascript/aarhustech++/scripts/modules/MinLaering.js
+++ javascript/aarhustech++/scripts/modules/MinLaering.js
@@ -1,3 +1,8 @@
+// todo:
+//   freetext - show user the recommended text (content)
+//   fix so it works on all questions in each section, not only the first one
+
+
 console.log("[MinLaering Mod] Initialized");
 
 var JSONData = {};
@@ -106,6 +111,7 @@ function FindAllSectionsAndQuestions() {
         if (!book_chapter_section_id.includes("section-"))
             continue;
 
+
         // data-type="exercise"
         var exercises = book_chapter_sections[i].querySelectorAll('[data-type="exercise"]');
         for (var j = 0; j < exercises.length; j++) {
@@ -113,95 +119,110 @@ function FindAllSectionsAndQuestions() {
             if (!exercise_id.includes("section-"))
                 continue;
 
+            console.log("[MinLaering Mod] Found exercise: " + exercise_id);
+
             // <div data-v-09f888e3="" id="exercise-question-64916" class="box is-paddingless has-border-radius-large has-background-grey-lightest">
-            var exercise_question = exercises[j].querySelector('[id^="exercise-question-"]');
+            var exercise_question = exercises[j].querySelectorAll('[id^="exercise-question-"]');
             if (exercise_question == null)
                 continue;
+
+            
+
+            console.log("[MinLaering Mod] Found exercise question: " + exercise_question.getAttribute('id'));
 
             var exercise_questions_id = exercise_question.getAttribute('id');
             if (!exercise_questions_id.includes("exercise-question-"))
                 continue;
 
-            // id="exercise-question-task-62929"
-            var exercise_question_task = exercise_question.querySelector('[id^="exercise-question-task-"]');
-            if (exercise_question_task == null)
-                continue;
-
-            var exercise_question_task_id = exercise_question_task.getAttribute('id');
-            if (document.getElementById("minlaeringmod-showanswer-button-" + exercise_question_task_id) != null)
-                continue;
-
-
-            var answer = GetAnswer(book_chapter_section_id, exercise_id, exercise_questions_id, exercise_question_task_id);
-
-            switch (answer.type) {
-                case "clickword": {
-                    for (var k = 0; k < answer.options.length; k++)
-                    {
-                        var correct_option = answer.options[k];
-                    
-                        // find div that has .option as its text content, and make it green
-                        var clickable_sections = exercise_question_task.getElementsByClassName("click-word-input");
-                        for (var l = 0; l < clickable_sections.length; l++) {
-                            var clickable_section = clickable_sections[l];
-                            if (clickable_section.textContent == " " + correct_option + " ") {
-                                clickable_section.setAttribute("style", "background-color: #AC4303;");
-                            }   
-                        }
-                    }
-                    break;
-                }
-
-                case "multiplechoice": {
-                    for (var k = 0; k < answer.options.length; k++)
-                    {
-                        var correct_option = answer.options[k];
-                    
-                        // find div that has .option as its text content, and make it green
-                        var clickable_sections = exercise_question_task.getElementsByClassName("multiple-choice-option");
-                        for (var l = 0; l < clickable_sections.length; l++) {
-                            var clickable_section = clickable_sections[l];
-                            // get class multiple-choice-title and then get text content from that
-                            var multiple_choice_title = clickable_section.getElementsByClassName("multiple-choice-title")[0];
-                            var multiple_choice_title_text = multiple_choice_title.textContent;
-                            if (multiple_choice_title_text == " " + correct_option + " ") {
-                                // class="multiple-choice-letter
-                                var multiple_choice_letter = clickable_section.getElementsByClassName("multiple-choice-letter")[0];
-                                multiple_choice_letter.setAttribute("style", "background-color: #AC4303;");
-                            }
-                        }
-                    }
-                    break;
-                }
-
-                case "dropdown": {
-                    /*
-                    <div class="dropdown-items"><div class="dropdown-item is-flex is-flex-wrap-nowrap is-align-items-center is-clickable is-size-7"><svg data-v-2e57dc73="" class="base-checkbox is-flex-shrink-0 mr-4 is-active has-background-blue is-medium"><use data-v-2e57dc73="" xlink:href="/img/icons.125a1316.svg#icon-checkbox-active"></use></svg><span class=""> - </span></div><div class="dropdown-item is-flex is-flex-wrap-nowrap is-align-items-center is-clickable is-size-7"><svg data-v-2e57dc73="" class="base-checkbox is-flex-shrink-0 mr-4 has-background-grey-lighterer is-medium"><use data-v-2e57dc73="" xlink:href="/img/icons.125a1316.svg#icon-checkbox-"></use></svg><span class=""> Gendrivelse 1 </span></div><div class="dropdown-item is-flex is-flex-wrap-nowrap is-align-items-center is-clickable is-size-7"><svg data-v-2e57dc73="" class="base-checkbox is-flex-shrink-0 mr-4 has-background-grey-lighterer is-medium"><use data-v-2e57dc73="" xlink:href="/img/icons.125a1316.svg#icon-checkbox-"></use></svg><span class=""> Gendrivelse 2 </span></div></div>
-                    */
-                    var dropdown_item = exercise_question_task.getElementsByClassName("dropdown-item");
-                    for (var k = 0; k < dropdown_item.length; k++) {
-                        var item = dropdown_item[k];
-
-                        // get span underneath and then get text content from that
-                        var span = item.getElementsByTagName("span")[0];
-                        var span_text = span.textContent;
-                        for (var l = 0; l < answer.options.length; l++) {
-                            if (span_text == " " + answer.options[l] + " ") {
-                                var svg = item.getElementsByTagName("svg")[0];
-                                var current_class = svg.getAttribute("class");
-                                current_class = current_class.replace("has-background-blue", "has-background-orange");
-                                current_class = current_class.replace("has-background-grey-lighterer", "has-background-orange");
-                                svg.setAttribute("class", current_class);
-                            }
-                        }
-                    }
-                    break;
-                }
-
-                default: {
-                    console.log("[MinLaering Mod] Unsupported question type: " + answer.type);
+                // get all exercise-question-64920
+            var exercise_question_tasks = exercise_question.querySelectorAll('[id^="exercise-question-task-"]');
+            console.log("[MinLaering Mod] Found " + exercise_question_tasks.length + " exercise question tasks");
+            for (var k = 0; k < exercise_question_tasks.length; k++) {
+                var exercise_question_task = exercise_question_tasks[k];
+                if (exercise_question_task == null)
                     continue;
+
+                console.log("[MinLaering Mod] Found exercise question task: " + exercise_question_task.getAttribute('id'));
+
+                var exercise_question_task_id = exercise_question_task.getAttribute('id');
+                if (document.getElementById("minlaeringmod-showanswer-button-" + exercise_question_task_id) != null)
+                    continue;
+
+                var answer = GetAnswer(book_chapter_section_id, exercise_id, exercise_questions_id, exercise_question_task_id);
+
+                switch (answer.type) {
+                    case "clickword": {
+                        for (var k = 0; k < answer.options.length; k++)
+                        {
+                            var correct_option = answer.options[k];
+                        
+                            // find div that has .option as its text content, and make it green
+                            var clickable_sections = exercise_question_task.getElementsByClassName("click-word-input");
+                            for (var l = 0; l < clickable_sections.length; l++) {
+                                var clickable_section = clickable_sections[l];
+                                if (clickable_section.textContent == " " + correct_option + " ") {
+                                    clickable_section.setAttribute("style", "background-color: #AC4303;");
+                                    console.log("[MinLaering Mod] (" + exercise_question_task_id + ") Found correct answer: " + correct_option);
+                                }   
+                            }
+                        }
+                        break;
+                    }
+
+                    case "multiplechoice": {
+                        for (var k = 0; k < answer.options.length; k++)
+                        {
+                            var correct_option = answer.options[k];
+                        
+                            // find div that has .option as its text content, and make it green
+                            var clickable_sections = exercise_question_task.getElementsByClassName("multiple-choice-option");
+                            for (var l = 0; l < clickable_sections.length; l++) {
+                                var clickable_section = clickable_sections[l];
+                                // get class multiple-choice-title and then get text content from that
+                                var multiple_choice_title = clickable_section.getElementsByClassName("multiple-choice-title")[0];
+                                var multiple_choice_title_text = multiple_choice_title.textContent;
+                                if (multiple_choice_title_text == " " + correct_option + " ") {
+                                    // class="multiple-choice-letter
+                                    var multiple_choice_letter = clickable_section.getElementsByClassName("multiple-choice-letter")[0];
+                                    multiple_choice_letter.setAttribute("style", "background-color: #AC4303;");
+                                    console.log("[MinLaering Mod] (" + exercise_question_task_id + ") Found correct answer: " + correct_option);
+                                }
+                            }
+                        }
+                        break;
+                    }
+
+                    case "dropdown": {
+                        /*
+                        <div class="dropdown-items"><div class="dropdown-item is-flex is-flex-wrap-nowrap is-align-items-center is-clickable is-size-7"><svg data-v-2e57dc73="" class="base-checkbox is-flex-shrink-0 mr-4 is-active has-background-blue is-medium"><use data-v-2e57dc73="" xlink:href="/img/icons.125a1316.svg#icon-checkbox-active"></use></svg><span class=""> - </span></div><div class="dropdown-item is-flex is-flex-wrap-nowrap is-align-items-center is-clickable is-size-7"><svg data-v-2e57dc73="" class="base-checkbox is-flex-shrink-0 mr-4 has-background-grey-lighterer is-medium"><use data-v-2e57dc73="" xlink:href="/img/icons.125a1316.svg#icon-checkbox-"></use></svg><span class=""> Gendrivelse 1 </span></div><div class="dropdown-item is-flex is-flex-wrap-nowrap is-align-items-center is-clickable is-size-7"><svg data-v-2e57dc73="" class="base-checkbox is-flex-shrink-0 mr-4 has-background-grey-lighterer is-medium"><use data-v-2e57dc73="" xlink:href="/img/icons.125a1316.svg#icon-checkbox-"></use></svg><span class=""> Gendrivelse 2 </span></div></div>
+                        */
+                        var dropdown_item = exercise_question_task.getElementsByClassName("dropdown-item");
+                        for (var k = 0; k < dropdown_item.length; k++) {
+                            var item = dropdown_item[k];
+
+                            // get span underneath and then get text content from that
+                            var span = item.getElementsByTagName("span")[0];
+                            var span_text = span.textContent;
+                            for (var l = 0; l < answer.options.length; l++) {
+                                if (span_text == " " + answer.options[l] + " ") {
+                                    var svg = item.getElementsByTagName("svg")[0];
+                                    var current_class = svg.getAttribute("class");
+                                    current_class = current_class.replace("has-background-blue", "has-background-orange");
+                                    current_class = current_class.replace("has-background-grey-lighterer", "has-background-orange");
+                                    svg.setAttribute("class", current_class);
+                                    console.log("[MinLaering Mod] (" + exercise_question_task_id + ") Found correct answer: " + answer.options[l]);
+                                }
+                            }
+                        }
+                        break;
+                    }
+
+                    default: {
+                        console.log("[MinLaering Mod] Unsupported question type: " + answer.type);
+                        continue;
+                    }
                 }
+                console.log("END")
             }
         }
     }
