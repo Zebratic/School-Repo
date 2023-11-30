2de81195527753e551236b2a2af5bff4983ca1af
Zebratic <zebraticyt@gmail.com>
2023-11-30 13:03:27 +0100

    MinLaering Mod Update
    + clickwords cheat
    + dropdown cheat

--- javascript/aarhustech++/scripts/modules/MinLaering.js
+++ javascript/aarhustech++/scripts/modules/MinLaering.js
@@ -4,50 +4,187 @@ var JSONData = {};
 
 var s = document.createElement('script');
 s.src = chrome.runtime.getURL('scripts/modules/intercepter.js');
-s.onload = function() {
+s.onload = function () {
     this.remove();
 };
 (document.head || document.documentElement).appendChild(s);
 
-
 document.addEventListener('MinLaeringMod_Data', function (e) {
-    var data = e.detail;
     var url = e.target.URL;
-
-    JSONData = JSON.parse(data);
+    JSONData = e.detail;
     console.log("[MinLaering Mod] Intercepted data from " + url);
     console.log(JSONData);
 });
 
 
+function GetAnswer(book_id, section_id, exercise_question_id, exercise_question_task_id) {
+    if (JSONData == null) {
+        console.log("[MinLaering Mod] Mod has not intercepted any data yet");
+        return;
+    }
+
+    console.log("[MinLaering Mod] Searching for BOOK:" + book_id + " SECTION:" + section_id + " EXERCISE:" + exercise_question_id + " TASK:" + exercise_question_task_id);
+
+    var book_id_int = parseInt(book_id.split('-')[1]); // section-XXXXX
+    var section_id_int = parseInt(section_id.split('-')[1]); // section-XXXXX
+    var exercise_question_id_int = parseInt(exercise_question_id.split('-')[2]); // exercise-question-XXXXX
+    var exercise_question_task_id = parseInt(exercise_question_task_id.split('-')[3]); // exercise-question-task-XXXXX
+
+    
+    var book = JSONData.data.sections.find(x => x.id === book_id_int);
+    if (book == null) {
+        console.log("[MinLaering Mod] Could not find book");
+        return;
+    }
+
+    var section = book.sections.find(x => x.id === section_id_int);
+    if (section == null) {
+        console.log("[MinLaering Mod] Could not find section");
+        return;
+    }
+
+    var question = section.sections.find(x => x.id === exercise_question_id_int);
+    if (question == null) {
+        console.log("[MinLaering Mod] Could not find question");
+        return;
+    }
+
+    console.log(question);
+    var task = question.tasks.find(x => x.id === exercise_question_task_id);
+    if (task == null) {
+        console.log("[MinLaering Mod] Could not find task");
+        return;
+    }
+
+    var answer = {
+        type: task.type.machine_name,
+        options: []
+    }
+
+    console.log("Question type: " + task.type.machine_name);
+    // answers are found in conf.options[].is_correct
+
+    switch (task.type.machine_name) {
+        case "clickword": {
+            var options_text = task.conf.options.find(x => x.is_correct === true).option;
+            answer.options.push(options_text);            
+            return answer;
+        }
 
 
+        case "dropdown": {
+            var dropdown_selections = task.conf.content; // "{-|Gendrivelse 1|Gendrivelse 2-correct}"
+            var dropdown_selections_split = dropdown_selections.split("|");
+            for (var i = 0; i < dropdown_selections_split.length; i++) {
+                if (dropdown_selections_split[i].includes("-correct}")) {
+                    var correct_answer = dropdown_selections_split[i].replace("-correct}", "");
+                    answer.options.push(correct_answer);
+                }
+            }
+            return answer;
+        }
+
+        case "freetext": return answer;
+            
 
 
-function GetAnswer(question_id) {
-
+        default: return answer;
+    }
 }
 
 function FindAllSectionsAndQuestions() {
-    var sections = document.querySelectorAll('[data-type="exercise"]');
-    console.log("Found " + sections.length + " sections");
-    
-    sections.forEach(function (section) {
-        var name = section.getAttribute("data-prefix");
-        var section_id = section.getAttribute("id");
-        console.log(name + ": " + section_id);
+    // data-type="book_chapter_section"
+    var book_chapter_sections = document.querySelectorAll('[data-type="book_chapter_section"]');
+    for (var i = 0; i < book_chapter_sections.length; i++) {
+        var book_chapter_section_id = book_chapter_sections[i].getAttribute('id');
+        if (!book_chapter_section_id.includes("section-"))
+            continue;
 
-        // get grammar-outer-container -> exercise-content -> pb-5 -> x amount of my-7
-        var questions = section.querySelectorAll('.my-7');
-        if (questions.length > 0)
-            console.log("Found " + questions.length + " questions");
-    });
+        // data-type="exercise"
+        var exercises = book_chapter_sections[i].querySelectorAll('[data-type="exercise"]');
+        for (var j = 0; j < exercises.length; j++) {
+            var exercise_id = exercises[j].getAttribute('id');
+            if (!exercise_id.includes("section-"))
+                continue;
+
+            // <div data-v-09f888e3="" id="exercise-question-64916" class="box is-paddingless has-border-radius-large has-background-grey-lightest">
+            var exercise_question = exercises[j].querySelector('[id^="exercise-question-"]');
+            if (exercise_question == null)
+                continue;
+
+            var exercise_questions_id = exercise_question.getAttribute('id');
+            if (!exercise_questions_id.includes("exercise-question-"))
+                continue;
+
+            // id="exercise-question-task-62929"
+            var exercise_question_task = exercise_question.querySelector('[id^="exercise-question-task-"]');
+            if (exercise_question_task == null)
+                continue;
+
+            var exercise_question_task_id = exercise_question_task.getAttribute('id');
+            if (document.getElementById("minlaeringmod-showanswer-button-" + exercise_question_task_id) != null)
+                continue;
+
+
+            var answer = GetAnswer(book_chapter_section_id, exercise_id, exercise_questions_id, exercise_question_task_id);
+
+            switch (answer.type) {
+                case "clickword": {
+                    for (var k = 0; k < answer.options.length; k++)
+                    {
+                        var correct_option = answer.options[k];
+                    
+                        // find div that has .option as its text content, and make it green
+                        var clickable_sections = exercise_question_task.getElementsByClassName("click-word-input");
+                        for (var l = 0; l < clickable_sections.length; l++) {
+                            var clickable_section = clickable_sections[l];
+                            if (clickable_section.textContent == " " + correct_option + " ") {
+                                clickable_section.setAttribute("style", "background-color: #AC4303;");
+                            }   
+                        }
+                    }
+                    break;
+                }
+
+                case "dropdown": {
+                    /*
+                    <div class="dropdown-items"><div class="dropdown-item is-flex is-flex-wrap-nowrap is-align-items-center is-clickable is-size-7"><svg data-v-2e57dc73="" class="base-checkbox is-flex-shrink-0 mr-4 is-active has-background-blue is-medium"><use data-v-2e57dc73="" xlink:href="/img/icons.125a1316.svg#icon-checkbox-active"></use></svg><span class=""> - </span></div><div class="dropdown-item is-flex is-flex-wrap-nowrap is-align-items-center is-clickable is-size-7"><svg data-v-2e57dc73="" class="base-checkbox is-flex-shrink-0 mr-4 has-background-grey-lighterer is-medium"><use data-v-2e57dc73="" xlink:href="/img/icons.125a1316.svg#icon-checkbox-"></use></svg><span class=""> Gendrivelse 1 </span></div><div class="dropdown-item is-flex is-flex-wrap-nowrap is-align-items-center is-clickable is-size-7"><svg data-v-2e57dc73="" class="base-checkbox is-flex-shrink-0 mr-4 has-background-grey-lighterer is-medium"><use data-v-2e57dc73="" xlink:href="/img/icons.125a1316.svg#icon-checkbox-"></use></svg><span class=""> Gendrivelse 2 </span></div></div>
+                    */
+                    var dropdown_item = exercise_question_task.getElementsByClassName("dropdown-item");
+                    for (var k = 0; k < dropdown_item.length; k++) {
+                        var item = dropdown_item[k];
+
+                        // get span underneath and then get text content from that
+                        var span = item.getElementsByTagName("span")[0];
+                        var span_text = span.textContent;
+                        for (var l = 0; l < answer.options.length; l++) {
+                            if (span_text == " " + answer.options[l] + " ") {
+                                var svg = item.getElementsByTagName("svg")[0];
+                                var current_class = svg.getAttribute("class");
+                                current_class = current_class.replace("has-background-blue", "has-background-orange");
+                                current_class = current_class.replace("has-background-grey-lighterer", "has-background-orange");
+                                svg.setAttribute("class", current_class);
+                            }
+                        }
+                    }
+                    break;
+                }
+
+                default: {
+                    console.log("[MinLaering Mod] Unknown question type");
+                    continue;
+                }
+            }
+        }
+    }
 }
-
-
-
 
 window.addEventListener('load', function () {
     setTimeout(FindAllSectionsAndQuestions, 3000); // Wait for page to load
-    setInterval(FindAllSectionsAndQuestions, 10000); // Check every 10 seconds
+    setInterval(FindAllSectionsAndQuestions, 3000); // Check every 3 seconds
+});
+
+// on every click on page, check if there are any new questions
+document.addEventListener('click', function (e) {
+    FindAllSectionsAndQuestions();
 });
--- javascript/aarhustech++/scripts/modules/intercepter.js
+++ javascript/aarhustech++/scripts/modules/intercepter.js
@@ -1,3 +1,7 @@
+var school_id = -1;
+var requestHeaders = {};
+var has_fetched_data = false;
+
 (function(xhr) {
     var XHR = XMLHttpRequest.prototype;
     var open = XHR.open;
@@ -16,16 +20,58 @@
     };
     XHR.send = function(postData) {
         this.addEventListener('load', function() {
-            var endTime = (new Date()).toISOString();
-            var myUrl = this._url ? this._url.toLowerCase() : this._url;
-            if(myUrl) {
-                if(myUrl.includes("api.minlaering.dk/schools/") && myUrl.includes("/books/") && myUrl.includes("/chapters/") && myUrl.includes("/sections/")) {
-                    var responseData = this.response;
-                    document.dispatchEvent(new CustomEvent('MinLaeringMod_Data', { url: myUrl, detail: responseData }));                  
+            requestHeaders = this._requestHeaders;
+
+            if (this._url) {
+                var url_match = this._url.match(/schools\/(\d+)/);
+                if (url_match != null) {
+                    school_id = url_match[1];
+                    console.log("School ID: " + school_id);
+                    if (has_fetched_data == false)
+                        FetchAssignmentData();
                 }
             }
         });
         return send.apply(this, arguments);
-    };
-
+    }; 
 })(XMLHttpRequest);
+
+function FetchAssignmentData() {
+    // current page url
+    var url = window.location.href;
+    // https://app.minlaering.dk/bog/46/kapitel/64912/sektion/64913#section-64915
+    // https://api.minlaering.dk/schools/254/books/46/chapters/64912/sections/64913
+    var book_id = url.match(/bog\/(\d+)/);
+    var chapter_id = url.match(/kapitel\/(\d+)/);
+    var section_id = url.match(/sektion\/(\d+)/);
+    if (book_id != null && chapter_id != null && section_id != null) {
+        book_id = book_id[1];
+        chapter_id = chapter_id[1];
+        section_id = section_id[1];
+    }
+    else {
+        console.log("Could not find book, chapter or section ID");
+        return;
+    }
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://api.minlaering.dk/schools/" + school_id + "/books/" + book_id + "/chapters/" + chapter_id + "/sections/" + section_id, true);
+    for (var header in requestHeaders) {
+        xhr.setRequestHeader(header, requestHeaders[header]);
+    }
+    
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4) {
+            var response = xhr.responseText;
+            var data = JSON.parse(response);
+            has_fetched_data = true;
+            document.dispatchEvent(new CustomEvent('MinLaeringMod_Data', { url: url, detail: data }));                  
+        }
+    }
+    xhr.send();
+}
+
+
+console.log("[MinLaering Mod] Intercepter.js injected successfully!");
+
