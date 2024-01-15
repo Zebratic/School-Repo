0ef5d7d23da571a925eed17f3158eed51ed431a9
Zebratic <zebraticyt@gmail.com>
2024-01-15 15:01:00 +0100

    sorting algorithms

---
+++ p5js/Sorters/bogosort.js
@@ -0,0 +1,58 @@
+let values = [];
+let i = 0;
+let j = 0;
+let sorted = false;
+
+function setup() {
+    createCanvas(400, 400);
+    values = new Array(width);
+    for (let i = 0; i < values.length; i++) {
+        values[i] = random(height);
+    }
+    frameRate(60); // Adjust the frame rate to slow down the animation
+}
+
+function draw() {
+    background(0);
+    
+    if (!sorted) {
+        if (isSorted(values)) {
+            sorted = true;
+            console.log("Finished sorting");
+            noLoop();
+            for (let i = values.length - 1; i >= 0; i--) {
+                stroke(0, 255, 0);
+                line(i, height, i, height - values[i]);
+            }
+        } else {
+            shuffleArray(values);
+        }
+    }
+    
+    for (let i = 0; i < values.length; i++) {
+        if (i === j || i === j + 1) {
+            stroke(0, 255, 0); // Change stroke color to green
+        } else {
+            stroke(255);
+        }
+        line(i, height, i, height - values[i]);
+    }
+}
+
+function shuffleArray(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
+
+function isSorted(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
+            return false;
+        }
+    }
+    return true;
+}
---
+++ p5js/Sorters/bubblesort.js
@@ -0,0 +1,50 @@
+let values = [];
+let i = 0;
+let j = 0;
+
+function setup() {
+    createCanvas(400, 400);
+    values = new Array(width);
+    for (let i = 0; i < values.length; i++) {
+        values[i] = random(height);
+    }
+    frameRate(60); // Adjust the frame rate to slow down the animation
+}
+
+function draw() {
+    background(0);
+    
+    if (i < values.length) {
+        for (let j = 0; j < values.length - i - 1; j++) {
+            let a = values[j];
+            let b = values[j + 1];
+            if (a > b) {
+                swap(values, j, j + 1);
+            }
+        }
+    } else {
+        console.log("Finished sorting");
+        noLoop();
+        for (let i = values.length - 1; i >= 0; i--) {
+            stroke(0, 255, 0);
+            line(i, height, i, height - values[i]);
+        }
+    }
+    
+    i++;
+    
+    for (let i = 0; i < values.length; i++) {
+        if (i === j || i === j + 1) {
+            stroke(0, 255, 0); // Change stroke color to green
+        } else {
+            stroke(255);
+        }
+        line(i, height, i, height - values[i]);
+    }
+}
+
+function swap(arr, a, b) {
+    let temp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = temp;
+}
